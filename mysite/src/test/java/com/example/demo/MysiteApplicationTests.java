97c4937be4a123df77870f7db730964a1d6a6110
GREEN <GREEN@G704-00>
2026-01-03 18:03:33 +0900

    Junit test code 실행

--- mysite/src/test/java/com/example/demo/MysiteApplicationTests.java
+++ mysite/src/test/java/com/example/demo/MysiteApplicationTests.java
@@ -1,13 +1,30 @@
 package com.example.demo;
 
+import java.time.LocalDateTime;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class MysiteApplicationTests {
-
+	
+	@Autowired
+	private QuestionRepository questionRepository;
+	
 	@Test
 	void contextLoads() {
+		Question q1 = new Question();
+		q1.setSubject("궁금합니다.");
+		q1.setContent("질문 내용입니다.");
+		q1.setCreateDate(LocalDateTime.now());
+		this.questionRepository.save(q1);
+		
+		Question q2 = new Question();
+		q2.setSubject("git공부가 필요합니다.");
+		q2.setContent("git에 대한 질문 내용입니다.");
+		q2.setCreateDate(LocalDateTime.now());
+		this.questionRepository.save(q2);
 	}
 
 }
